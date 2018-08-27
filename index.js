6950ce22dcb0ea51a61f1d0966a93f3e1d1767ac
Leonardo Gatica <lgatica@protonmail.com>
2018-08-27 12:35:15 -0300

    feat(rancher): add finish upgrade

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const axios = require('axios')
 const merge = require('lodash.merge')
+const pWhilst = require('p-whilst')
 
 const getStacks = async (client) => {
   return (await client.get('environments/')).data.data
@@ -72,6 +73,35 @@ const upgradeService = async (client, id, upgrade) => {
   return (await client.post(`services/${id}/?action=upgrade`, upgrade)).data
 }
 
+const checkUpgradeService = async (client, id) => {
+  let attempts = 0
+  let service = {}
+  const upgradeTimeout = 5 * 60
+  const sleep = timeout => new Promise(resolve => setTimeout(() => resolve(), timeout))
+  return await pWhilst(
+    () => {
+      return service.state !== 'upgraded'
+    },
+    async () => {
+      await sleep(2)
+      attempts += 2
+      if (attempts > upgradeTimeout) {
+        throw new Error('A timeout occured while waiting for Rancher to finish the previous upgrade')
+      }
+      try {
+        const response = await client.get(`services/${id}`)
+        service = response.data
+      } catch (err) {
+        throw new Error('Unable to request the service status from the Rancher API')
+      }
+    }
+  )
+}
+
+const finishUpgradeService = async (client, id) => {
+  return (await client.post(`services/${id}/?action=finishupgrade`)).data
+}
+
 class Rancher {
   constructor (options) {
     this.config = Object.assign({
@@ -106,8 +136,16 @@ class Rancher {
       upgrade = await withAdditionalEnvVar(upgrade, releaseVariable, version)
     }
 
-    const response = (!dryRun)
+    let response = (!dryRun)
       ? await upgradeService(this.client, service.id, upgrade)
+      : {}
+
+    response = (!dryRun)
+      ? await checkUpgradeService(this.client, service.id, upgrade)
+      : {}
+
+    response = (!dryRun)
+      ? await finishUpgradeService(this.client, service.id, upgrade)
       : {}
 
     return { service, upgrade, response }
